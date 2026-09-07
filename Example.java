cc8bc1fcce3e7dac28073741a6678ba3ffbf026d
Jindae <jindae.kim@seoultech.ac.kr>
2026-09-07 21:02:33 +0900

    Add a Node example.

---
+++ Example.java
@@ -0,0 +1,17 @@
+public class Example {
+
+    public static void main(String[] args) {
+        Node root = new Node("Root");
+        Node child1 = new Node("Child 1");
+        Node child2 = new Node("Child 2");
+
+        root.addChild(child1);
+        root.addChild(child2);
+
+        System.out.println("Root label: " + root.label);
+        System.out.println("Children of Root:");
+        for (Node child : root.children) {
+            System.out.println("- " + child.label);
+        }
+    }
+}
---
+++ Node.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+    public String label;
+    public Node parent;
+    public List<Node> children;
+
+    public Node(String label) {
+        this.label = label;
+        this.children = new ArrayList<>();
+    }
+
+    public void addChild(Node child) {
+        this.children.add(child);
+    }
+}
